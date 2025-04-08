a043804fc1d4c494b9bc172638beb51735615f76
heathmcgregor <42739945+heathmcgregor@users.noreply.github.com>
2025-04-09 08:22:00 +1000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,3 @@
 basic.forever(function () {
-    if (bosonKit.bosonAnalogRead(AnalogPin.P2, bosonKit.BosonSensorAnalogRead.BosonRotation) < 125) {
-        basic.showIcon(IconNames.Yes)
-    } else {
-        basic.showIcon(IconNames.No)
-    }
-    basic.pause(1000)
-    if (bosonKit.bosonAnalogRead(AnalogPin.P0, bosonKit.BosonSensorAnalogRead.BosonLightIntensity) < 125) {
-        basic.showIcon(IconNames.Happy)
-    } else {
-        basic.showIcon(IconNames.Sad)
-    }
+	
 })
